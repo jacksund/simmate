b7e3c0092022a69407da33fc92f1f1db7df07497
jacksund <jacksundberg123@gmail.com>
2020-12-09 21:36:18 -0500

    initial commit

---
+++ website/manageinpython.py
@@ -0,0 +1,18 @@
+
+#############################################################################
+### The code below is the equiv of running 'python manage.py shell'
+#############################################################################
+
+import os
+os.environ.setdefault('DJANGO_SETTINGS_MODULE','puttyexams.settings')
+
+import django
+django.setup()
+
+del django
+del os
+
+#############################################################################
+### Now you can import apps from your project and run any django commands
+### Just include "import manageinpython" at the start of your script
+#############################################################################
---
+++ website/website/views.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+from django.shortcuts import render
+
+def home(request):
+    context = {}
+    template = 'website/home.html' #!!! Doesn't exist at the moment
+    return render(request, template, context)
