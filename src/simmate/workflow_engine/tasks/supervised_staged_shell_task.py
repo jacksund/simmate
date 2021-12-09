5064ac8823a1c7f062ceda5ad0434733086f9d03
jacksund <jacksundberg123@gmail.com>
2021-12-09 13:51:35 -0500

    :plunger: add docs and typing to ssstask

--- src/simmate/workflow_engine/tasks/supervised_staged_shell_task.py
+++ src/simmate/workflow_engine/tasks/supervised_staged_shell_task.py
@@ -18,16 +18,6 @@ from typing import List, Any
 from pymatgen.core.structure import Structure
 from simmate.workflow_engine.error_handler import ErrorHandler
 
-
-# This is a combination of prefect ShellTask, custodian Job, and Custodian main.
-
-# from abc import ABC, abstractmethod
-# This is really an abstract class that should be overwritten, but I can't have the
-# class inherit from both prefect Task and abc's AbstractBaseClass. Therefore,
-# I don't strictly enforce writting new setup/execute/postprocess methods.
-# I instead trust the user to know what they are doing when inheriting from
-# this class (maybe I can fix this in the future.)
-
 # cleanup_on_fail=False, # TODO I should add a Prefect state_handler that can
 # reset the working directory between task retries -- in some cases we may
 # want to delete the entire directory.
@@ -39,6 +29,92 @@ from simmate.workflow_engine.error_handler import ErrorHandler
 
 
 class SupervisedStagedShellTask(Task):
+    """
+    Introduction to the "SSS Task"
+    ------------------------------
+    This class contains the core functionality to *supervise* a *staged* task
+    involving some *shell* command.
+
+    Let's breakdown what this means...
+
+    A *shell* command is a single call to some external program. For example,
+    VASP requires that we call the "vasp > vasp.out" command in order to run a
+    calculation. We consider calling external programs a *staged* task made
+    up of three steps:
+
+        - setup = writing any input files required for the program
+        - execution = actually calling the command and running our program
+        - workup = loading data from output files back into python
+
+    And for *supervising* the task, this means we monitor the program while the
+    execution stage is running. So once a program is started, Simmate can check
+    output files for common errors/issues. If an error is found, we stop the
+    program, fix the issue, and then restart it. Any fixes that were made are
+    written to "simmate_corrections.csv".
+
+    This entire process (the different stages and monitoring) is carried out
+    using the ``run()`` method. You rarely use this class directly. Instead,
+    you typically use a subclass of it. As a user, you really just need to do
+    something like this:
+
+    .. code-block:: python
+
+       from simmate.calculator.example.tasks import ExampleTask
+       my_task = ExampleTask()
+       my_result = my_task.run()
+
+    And that's it!
+
+    For experts, this class can be viewed as a combination of prefect's ShellTask,
+    a custodian Job, Custodian monitoring. When subclassing this, we can absorb
+    functionality of pymatgen.io.vasp.sets too. By merging all of these together
+    into one class, we make things much easier for users and creating new Tasks.
+
+    Inheriting from this class
+    --------------------------
+    This class is commonly used to make tasks for our calculator modules. For
+    a full (and advanced) example, take a look at...
+
+    >>> simmate.calculators.vasp.tasks.base.VaspTask
+
+    And here's an example of inheriting and then running a task:
+
+    .. code-block:: python
+
+        from simmate.workflow_engine.tasks.supervised_staged_shell_task import (
+            SupervisedStagedShellTask as SSSTask,
+        )
+        from example.error_handlers import PossibleError1, PossibleError2
+
+
+        class ExampleTask(SSSTask):
+
+            command = "echo example"  # just prints out "example"
+            max_corrections = 7
+            error_handlers = [PossibleError1, PossibleError2]
+            polling_timestep = 0.1
+            monitor_freq = 10
+            some_new_setting = 123
+
+            def setup(self, structure, directory):  # <-- MUST have these two args
+                print("I'm setting things up!")
+                print(f"My new setting is {some_new_setting}")
+
+            def workup(self, directory):  # <-- MUST have this arg
+                print("I'm working things up!")
+
+
+        task = ExampleTask()
+        result = task.run()
+
+    There are a couple things to note here:
+
+    - It's optional to set/overwrite attributes. You can also add new ones too.
+    - It's optional to write a new __init__, setup, or workup methods
+    - make sure you include the structure/directory inputs, even if you don't use them.
+    - Don't add new kwargs to methods. Instead handle these options through attributes.
+
+    """
 
     # set a defualt command associated with this specific ShellTask
     # I set this here so that I don't have to copy/paste the init method
@@ -96,76 +172,71 @@ class SupervisedStagedShellTask(Task):
         save_corrections_to_file: bool = True,
         corrections_filename: str = "simmate_corrections.csv",
         empty_directory_on_finish: bool = False,
-        files_to_keep: List[str] = None,  
+        files_to_keep: List[str] = None,
         compress_output: bool = False,
         **kwargs: Any,
     ):
         """
-        
-        To create an instance:
 
-        >>> my_task = SupervisedStagedShellTask()
-
-        All parameters are optional, and if they aren't provided, default values
-        are inherited from class attributes.
-        
-        External hyperlinks, like Simmate_.
-        
-        .. _Simmate: http://www.python.org/
-        
-        
-        Example formula:
-        
-        .. math:: e^{i\pi} + 1 = 0
-        
-        Example list:
-
-        - test 123
-        
-        ``test``
-        
-        .. code-block:: python
-
-           import numpy
-           x = numpy.array([1,2,3])
-        
-        TEST::
-            something1
-            something 2 + 5
+        Creates a task instance of this class. The parameters passed will be the
+        same every time you call the task.run() method.
 
         Parameters
         ----------
         structure : pymatgen.core.structure.Structure (optional)
-            The structure to use for the task. 
-        command : str, optional
-            DESCRIPTION. The default is None.
-        directory : str, optional
-            DESCRIPTION. The default is None.
-        error_handlers : List[ErrorHandler], optional
-            DESCRIPTION. The default is None.
-        max_corrections : int, optional
-            DESCRIPTION. The default is None.
-        monitor : bool, optional
-            DESCRIPTION. The default is None.
-        polling_timestep : float, optional
-            DESCRIPTION. The default is None.
-        monitor_freq : int, optional
-            DESCRIPTION. The default is None.
-        save_corrections_to_file : bool, optional
-            DESCRIPTION. The default is True.
-        corrections_filename : str, optional
-            DESCRIPTION. The default is "simmate_corrections.csv".
-        empty_directory_on_finish : bool, optional
-            DESCRIPTION. The default is False.
-        files_to_keep : List[str], optional
-            DESCRIPTION. The default is None.
-        compress_output : bool, optional
-            DESCRIPTION. The default is False.
+            The structure to use for the task, if one is required. Typically, this
+            class is ran for multiple structures, where you can pass this
+            option to the task.run() method instead.
+        command : str (optional)
+            The command that will be called during execution.
+        directory : str (optional)
+            The directory to run everything in. This is passed to the ulitities
+            function simmate.ulitities.get_directory
+        error_handlers : List[ErrorHandler] (optional)
+            The list of error handler objects to use. These should be listing in
+            order of priority, where to highest priority is first. If one handler
+            is triggered, the correction will be applied and none of the
+            following handlers will be checked.
+        max_corrections : int (optional)
+            The maximum number of times that corrections will be made before
+            giving up on the calculation. Note, once this limit is exceeded, the
+            error is stored without correcting or restarting the run.
+        monitor : bool (optional)
+            Whether to run monitor handlers while the command runs. False means
+            wait until the job has completed.
+        polling_timestep : float (optional)
+            If we are monitoring the job for errors while it runs, this is how often
+            (in seconds) we should check the status of our job. Note this check is
+            just whether the job is done or not. This is NOT how often we check for
+            errors. See monitor_freq for that.
+        monitor_freq : int (optional)
+            The frequency we should run check for errors with our monitors. This is
+            based on the polling_timestep loops. For example, if we have a
+            polling_timestep of 10 seconds and a monitor_freq of 2, then we would run
+            the monitor checks every other loop -- or every 2*10 = 20 seconds. The
+            default values of polling_timestep=10 and monitor_freq=30 indicate that
+            we run monitoring functions every 5 minutes (10*30=300s=5min).
+        save_corrections_to_file : bool (optional)
+            Whether to write a log file of the corrections made. The default is True.
+        corrections_filename : str (optional)
+            If save_corrections_to_file is True, this is the filename of where
+            to write the corrections. The default is "simmate_corrections.csv".
+        empty_directory_on_finish : bool (optional)
+            Whether to delete all directory contents at the end of the task (after
+            workup), or not. This is only used to save on file space The default
+            is False.
+        files_to_keep : List[str] (optional)
+            If empty_directory_on_finish is True, the deletion process will skip
+            all files in this list is of filenames.
+        compress_output : bool (optional)
+            Whether to compress the directory to a zip file at the end of the
+            task run. This is done BEFORE empty_directory_on_finish and
+            files_to_keep are used. The default is False.
         **kwargs : Any
-            DESCRIPTION.
+            All extra arguments supported by prefect.core.task.Task
 
         """
-        
+
         # this is a common input but not required for all SSSTasks
         # core parts
         # settings for the stagedtask supervision/monitoring
@@ -173,7 +244,7 @@ class SupervisedStagedShellTask(Task):
         # only used if empty_directory_on_finish=True
         # To support other Prefect input options. To see all the options, visit...
         # https://docs.prefect.io/api/latest/core/task.html
-        
+
         # if any of these input parameters were given, overwrite the default
         # Note to python devs: this odd formatting is because we set our defaults
         # to None in __init__ while our actual default values are defined above
@@ -208,21 +279,55 @@ class SupervisedStagedShellTask(Task):
         # now inherit the parent Prefect Task class
         super().__init__(**kwargs)
 
-    def setup(self, structure, directory):
+    def setup(self, structure: Structure, directory: str):
         """
-        This abstract method is ran before the start of a job. Allows for some
-        pre-processing. This includes creating a directory, writing input files
-        or any other function ran before calling the executable.
+        This abstract method is ran before the command is actually executed. This
+        allows for some pre-processing, such as writing input files or any other
+        analysis.
+
+        You should never call this method directly unless you are debugging. This
+        is becuase setup() is normally called within the run() method.
+
+        Some tasks don't require a setup() method, so by default, this method
+        doesn't nothing but "pass".
+
+        Parameters
+        ----------
+        structure : pymatgen.core.structure.Structure
+            The structure to use for the task, if one is required.
+        directory : str
+            The directory to run everything in.
         """
         # Be sure to include directory and structure (or **kwargs) as input arguments
         # for higher-level compatibility with the run method.
         # You should never need to call this method directly!
         pass
 
-    def execute(self, directory, command):
+    def execute(self, directory: str, command: str):
+        """
+        This calls the command within the target directory and handles all error
+        handling as well as monitoring of the job.
 
-        # Establish the working directory for this run.
-        directory = get_directory(directory)
+        You should never call this method directly unless you are debugging. This
+        is becuase execute() is normally called within the run() method.
+
+        Some tasks don't require a setup() method, so by default, this method
+        does nothing but "pass".
+
+        Parameters
+        ----------
+        directory : str
+            The directory to run everything in.
+        command : str
+            The command that will be called during execution.
+
+        Returns
+        -------
+        corrections : List
+            A list of tuples where each entry is a error identified and the
+            correction applied. Ex: [("ExampleError", "ExampleCorrection")]
+
+        """
 
         # some error_handlers run while the shelltask is running. These are known as
         # Monitors and are labled via the is_monitor attribute. It's good for us
@@ -410,7 +515,7 @@ class SupervisedStagedShellTask(Task):
         return corrections
 
     @staticmethod
-    def _terminate_job(process, command):
+    def _terminate_job(process: subprocess.Popen, command: str):
         """
         Stopping the command we submitted can be a tricky business if we are running
         scripts in parallel (such as using mpirun). Different computers and OSs
@@ -421,9 +526,15 @@ class SupervisedStagedShellTask(Task):
         processes, you can overwrite this method as well.
 
         Users should never call this directly becuase this is instead applied
-        within the execute() method above.
+        within the execute() method.
 
-
+        Parameters
+        ----------
+        process : subprocess.Popen
+            The process object that will be terminated.
+        command : str
+            the command used to launch the process. This is sometimes useful
+            when searching for all running processes under this name.
         """
         # The normal line to end a popen process is just...
         #   process.terminate()
@@ -450,19 +561,48 @@ class SupervisedStagedShellTask(Task):
         # directory, which will make it so they don't have to overwrite all the
         # classes that inherit from this one.
 
-    def workup(self, directory):
+    def workup(self, directory: str):
         """
         This method is called at the end of a job, *after* error detection.
         This allows post-processing, such as cleanup, analysis of results,
         etc. This should return the result of the entire job, such as a
         the final structure or final energy calculated.
+
+        You should never call this method directly unless you are debugging. This
+        is becuase workup() is normally called within the run() method.
+
+        Some tasks don't require a workup() method, so by default, this method
+        does nothing but "pass".
+
+        Parameters
+        ----------
+        directory : str
+            The directory to run everything in.
+
         """
         # Be sure to include directory (or **kwargs) as input argument for
         # higher-level compatibility with the run method and SupervisedStagedTask
         # You should never need to call this method directly!
         pass
 
-    def postprocess(self, directory):
+    def postprocess(self, directory: str):
+        """
+        This method (if requested) compresses the directory and removes files from
+        it. The parameters that control it's behavior are set as attributes
+        or in the __init__. This includes:
+
+        - empty_directory_on_finish
+        - files_to_keep
+        - compress_output
+
+        You should never call this method directly unless you are debugging. This
+        is becuase postprocess() is normally called within the run() method.
+
+        Parameters
+        ----------
+        directory : str
+            The directory to run everything in.
+        """
         # compress the output directory for storage if requested
         if self.compress_output:
             make_archive(
@@ -494,15 +634,39 @@ class SupervisedStagedShellTask(Task):
     @defaults_from_attrs("structure", "directory", "command")
     def run(
         self,
-        structure=None,
-        directory=None,
-        command=None,
+        structure: Structure = None,
+        directory: str = None,
+        command: str = None,
     ):
         """
-        Runs the entire job in the current working directory without any error
-        handling. If you want robust error handling, then you should instead
-        run this through the SupervisedJobTask class. This method should
-        very rarely be used!
+        Runs the entire staged task (setup, execution, workup), which includes
+        supervising during execution.
+
+        Call this method once you have your task initialized. For each run you
+        can provide a new structure, directory, or command. For example,
+
+        .. code-block:: python
+
+           from simmate.calculator.example.tasks import ExampleTask
+           my_task = ExampleTask()
+           my_result = my_task.run(structure=my_structure, command=my_command)
+
+        Parameters
+        ----------
+        structure : pymatgen.core.structure.Structure (optional)
+            The structure to use for the task, if one is required.
+        command : str (optional)
+            The command that will be called during execution.
+        directory : str (optional)
+            The directory to run everything in. This is passed to the ulitities
+            function simmate.ulitities.get_directory
+
+        Returns
+        -------
+        dict
+            a dictionary of the result, corrections, and working directory used
+            for this task run
+
         """
 
         # because the command is something that is frequently changed at the
