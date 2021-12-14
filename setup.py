25345f736ba44e7771d281621193a863ddf448eb
Jack Sundberg <47992949+jacksund@users.noreply.github.com>
2021-12-14 12:59:28 -0500

    :robot: update version to 0.0.0.dev2

--- setup.py
+++ setup.py
@@ -65,7 +65,7 @@ setup(
     name="simmate",
     # Versions should comply with PEP 440:
     # https://www.python.org/dev/peps/pep-0440/
-    version="0.0.0.dev1",
+    version="0.0.0.dev2",
     # a quick summary and then README
     description="Simmate is a toolbox for computational materials research.",
     long_description=long_description,
