e0d1a3d3c2555bd7a035ad556bfd5b0f0969145d
jacksund <jacksundberg123@gmail.com>
2022-01-13 16:35:34 -0500

    :star2: add dynamics and prebader

--- src/simmate/calculators/vasp/tasks/dynamics.py
+++ src/simmate/calculators/vasp/tasks/dynamics.py
@@ -1,21 +1,17 @@
 # -*- coding: utf-8 -*-
 
-from simmate.calculators.vasp.tasks.relaxation.third_party.mit import MITRelaxationTask
+from simmate.calculators.vasp.tasks.relaxation.mit import MITRelaxation
 
-from simmate.calculators.vasp.error_handlers.all import (
-    TetrahedronMesh,
-    Eddrmm,
-    NonConvergingErrorHandler,
-)
+# This class used pymatgen's MITMDSet as it basis for settings.
 
 
-class MITDynamicsTask(MITRelaxationTask):
+class MITDynamicsTask(MITRelaxation):
 
     # The settings used for this calculation are based on the MITRelaxation, but
     # we are updating/adding new settings here.
     # !!! we hardcode temperatures and time steps here, but may take these as inputs
     # in the future
-    incar = MITRelaxationTask.incar.copy()
+    incar = MITRelaxation.incar.copy()
     incar.update(
         dict(
             TEBEG=300,  # start temperature
@@ -32,17 +28,15 @@ class MITDynamicsTask(MITRelaxationTask):
             BMIX=1,
             MAXMIX=20,
             NELM=500,
-            NSIM=4,
-            ISYM=0,
-            ISIF=0,
-            IBRION=0,
+            NSIM=4,  # number of bands to run in parallel  # !!! Should this be moved to config?
+            ISYM=0,  # turn off symmetry
+            ISIF=0,  # only update atom sites
+            IBRION=0,  # turns on molecular dynamics
             NBLOCK=1,
             KBLOCK=100,
             SMASS=0,
-            POTIM=2,  # time step
+            POTIM=2,  # time step (in fs)
             PREC="Low",
-            # ISPIN=2,  # same as before
-            LDAU=False,
         )
     )
     # because we no longer use LDAU, we can also remove all relevent settings from
@@ -52,11 +46,6 @@ class MITDynamicsTask(MITRelaxationTask):
     incar.pop("multiple_keywords__smart_ismear")
     incar.pop("EDIFF")
 
-    # We reduce the number of error handlers used on dynamics because many
-    # error handlers are based on finding the global minimum & converging, which
-    # not what we're doing with dynmaics
-    error_handlers = [
-        TetrahedronMesh(),
-        Eddrmm(),
-        NonConvergingErrorHandler(),
-    ]
+    # For now, I turn off all error handlers.
+    # TODO
+    error_handlers = []
