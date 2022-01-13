5d1407b29ae65fa76cd62772d44585fe18725575
jacksund <jacksundberg123@gmail.com>
2022-01-13 10:19:47 -0500

    :droplet: add temp s3tasks to workflows. remove sub directories

--- src/simmate/workflows/all.py
+++ src/simmate/workflows/all.py
@@ -1,7 +1,13 @@
 # -*- coding: utf-8 -*-
 
-from simmate.workflows.energy import energy_mit, energy_quality04, energy_matproj
-from simmate.workflows.relaxation import (
+# TODO: when I add more calculators, I can do something like this...
+# if "simmate.calculators.vasp" in installed_apps:
+from simmate.calculators.vasp.workflows.energy.all import (
+    energy_mit,
+    energy_quality04,
+    energy_matproj,
+)
+from simmate.calculators.vasp.workflows.relaxation.all import (
     relaxation_mit,
     relaxation_matproj,
     relaxation_quality00,
@@ -10,4 +16,13 @@ from simmate.workflows.relaxation import (
     relaxation_quality03,
     relaxation_quality04,
     relaxation_staged,
+)
+
+# These are tasks that are in early development and don't have databases tables.
+# Therefore, they are s3tasks only - not workflows.
+from simmate.calculators.vasp.tasks.band_structure import (
+    MaterialsProjectBandStructureTask as band_structure_matproj,
+)
+from simmate.calculators.vasp.tasks.density_of_states import (
+    MaterialsProjectDensityOfStatesTask as density_of_states_matproj,
 )
