f55f3135952c7d2d17f353a7fa0951e0b57506f2
Jack Sundberg <jacksundberg123@gmail.com>
2022-04-09 19:37:35 -0400

    :fish: add placeholders for interface

--- src/simmate/configuration/example_project/example_app/urls.py
+++ src/simmate/configuration/example_project/example_app/urls.py
@@ -1,5 +1,3 @@
-# -*- coding: utf-8 -*-
-
 """
 Want to make a website interface for your app? Then you'll need to fill this file
 out! If you ever venture to this level, we strongly recommend you go through the
@@ -9,10 +7,14 @@ Note, we are working to have Simmate build this file for you automatically, but
 that feature is still under early testing.
 """
 
-from django.shortcuts import render
+from django.urls import path
 
+from .views import example_view
 
-def example_view(request):
-    context = {}
-    template = "path/to/my/template.html"
-    return render(request, template, context)
+urlpatterns = [
+    path(
+        route="",
+        view=example_view,
+        name="example_app",
+    ),
+]
