dd3077569427ec1531febfb3200a855d2d99b47a
Jack Sundberg <jacksundberg123@gmail.com>
2022-04-24 20:56:15 -0400

    :snake: bump version and run pdoc

--- setup.py
+++ setup.py
@@ -75,7 +75,7 @@ setup(
     name="simmate",
     # Versions should comply with PEP 440:
     # https://www.python.org/dev/peps/pep-0440/
-    version="0.3.0",
+    version="0.4.0",
     # a quick summary and then README
     description="Simmate is a toolbox for computational materials research.",
     long_description=long_description,
