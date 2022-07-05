e1a9986c8f8e90e4a32cb9f46a6786e4c82680eb
Jack Sundberg <jacksundberg123@gmail.com>
2022-07-05 15:34:44 -0400

    add init text

---
+++ src/simmate/toolkit/structure_prediction/__init__.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+
+from .known import get_known_structures
+from .prototypes import get_structures_from_prototypes
+from .substitution import get_structures_from_substitution_of_known
