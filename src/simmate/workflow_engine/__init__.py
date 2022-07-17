a5ed526ba264ed3c4e30c6da693cf033f679eb4b
Jack Sundberg <jacksundberg123@gmail.com>
2022-07-17 15:36:05 -0400

    add simmate worker

--- src/simmate/workflow_engine/__init__.py
+++ src/simmate/workflow_engine/__init__.py
@@ -9,6 +9,7 @@ __doc__ = get_doc_from_readme(__file__)
 # module, so it may be worth isolating them for faster imports.
 from .error_handler import ErrorHandler
 from .supervised_staged_shell_task import S3Task
+from .worker import Worker
 
 # All imports below this point depend on the simmate.database module and therefore
 # need a database connection. This line sets up the database connection so
