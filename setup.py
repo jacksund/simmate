cc7fdcee3d54dc70bd2bb547bae29f64ac270a4b
Jack Sundberg <jacksundberg123@gmail.com>
2022-07-19 17:17:57 -0400

    update pyproject

--- setup.py
+++ setup.py
@@ -1,7 +1,13 @@
+# -*- coding: utf-8 -*-
 
-# This file is only required for editable installs using pip. We can
-# remove this file once the following issue is closed:
-#   https://github.com/pypa/setuptools/issues/2816
+"""
+This file is only required for editable installs using pip. We can
+remove this file once the following issue is closed:
+  https://github.com/pypa/setuptools/issues/2816
+
+As extra, the example_project/setup.py can also be converted to pyproject.toml
+and removed. I hold off on this to minimize new files made for users.
+"""
 
 from setuptools import setup
 
