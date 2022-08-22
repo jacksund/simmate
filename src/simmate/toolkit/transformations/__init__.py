a4761d6d9e2b396fa3e37c8e0b9ff14e1844e4f6
Jack Sundberg <jacksundberg123@gmail.com>
2022-08-22 10:52:08 -0400

    add extreme sym

--- src/simmate/toolkit/transformations/__init__.py
+++ src/simmate/toolkit/transformations/__init__.py
@@ -3,4 +3,5 @@
 from .atomic_permutation import AtomicPermutation
 from .coordinate_perturation_ordered import CoordinateOrderedPerturbation
 from .coordinate_perturbation import CoordinatePerturbation
+from .extreme_symmetry import ExtremeSymmetry
 from .lattice_strain import LatticeStrain
