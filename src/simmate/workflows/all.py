aee367029e9fe44c13394af15313a9544fd92017
Jack Sundberg <jacksundberg123@gmail.com>
2022-09-09 08:09:10 -0400

    add all-flows module

--- src/simmate/workflows/all.py
+++ src/simmate/workflows/all.py
@@ -1,1 +1,21 @@
 # -*- coding: utf-8 -*-
+
+"""
+This module provides a single endpoint where all registered workflows can be
+accessed for user-convenience. 
+
+
+"""
+
+# note, everything starts with an underscore because I don't want these variables
+# to be visable -- only the workflow classes.
+# The code below is modified from...
+#   https://stackoverflow.com/questions/30986470/
+
+from simmate.workflows.utilities import get_all_workflows as _get_all
+
+_workflows = _get_all()
+_module = globals()
+
+for _workflow in _workflows:
+    _module[_workflow.__name__] = _workflow
