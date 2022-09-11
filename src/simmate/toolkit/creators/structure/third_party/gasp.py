b6d448a647b752a5f2bad71f9c5e8e87754dc96d
Jack Sundberg <jacksundberg123@gmail.com>
2022-09-11 14:02:36 -0400

    add gasp and ase creators

---
+++ src/simmate/toolkit/creators/structure/third_party/gasp.py
@@ -0,0 +1,78 @@
+# -*- coding: utf-8 -*-
+
+import numpy
+
+from simmate.toolkit import Structure, Composition
+from simmate.toolkit.creators.structure.base import StructureCreator
+
+
+class GaspStructure(StructureCreator):
+    """
+    Create structures using the GASP package.
+
+    see source: https://github.com/henniggroup/GASP-python
+    see tutorials: https://github.com/henniggroup/GASP-python/blob/master/docs/usage.md
+    """
+
+    # DEV NOTE:
+    # The documentation and code organization is pretty difficult to follow for GASP,
+    # so I don't include any of their options. Instead, I assume all default values
+    # for a fixed composition. There's not much I can do about this until GASP fixes
+    # the organization of their code and add documentation. As-is, GASP is not ment to
+    # be used as a python module, which really hinders its reusability.
+
+    def __init__(self, composition: Composition):
+
+        try:
+            from gasp.development import Constraints
+            from gasp.general import CompositionSpace, IDGenerator
+            from gasp.organism_creators import RandomOrganismCreator
+        except ModuleNotFoundError:
+            raise ModuleNotFoundError(
+                "You must have GASP installed to use GaspStructure. "
+                "It must be installed from source: "
+                "https://github.com/henniggroup/GASP-python"
+            )
+
+        # generate the inputs required for GASP to make a new structure
+        # I assume all defaults for GASP right now
+        self.composition_space = CompositionSpace([composition.formula])
+        self.constraints = Constraints(None, self.composition_space)
+        self.id_generator = IDGenerator()
+        self.random_org_creator = RandomOrganismCreator(
+            None,
+            self.composition_space,
+            self.constraints,
+        )
+
+        # this creator doesnt use sym so P1 is the only option
+        self.spacegroup_options = [1]
+
+    def create_structure(self) -> Structure:
+
+        # sometimes gasp fails to make a structure, but let's loop it until
+        # we get a valid one
+        structure_gasp = False
+        while not structure_gasp:
+            # now make the new structure using
+            #   gasp.organism_creators.RandomOrganismCreator
+            structure_gasp = self.random_org_creator.create_organism(
+                self.id_generator,
+                self.composition_space,
+                self.constraints,
+                numpy.random,
+            )
+
+        # Grab the cell object from the output
+        structure_gasp = structure_gasp.cell
+
+        # convert the gasp Cell object to pymatgen Structure
+        # Cell is really a child of Structure, so I'm actually
+        # reducing functionality here
+        structure = Structure(
+            lattice=structure_gasp.lattice,
+            species=structure_gasp.species,
+            coords=structure_gasp.frac_coords,
+        )
+
+        return structure
--- src/simmate/toolkit/creators/structure/third_party/pyxtal.py
+++ src/simmate/toolkit/creators/structure/third_party/pyxtal.py
@@ -1,35 +1,46 @@
 # -*- coding: utf-8 -*-
 
+import logging
+
 from numpy.random import choice
 
+from simmate.toolkit import Composition, Structure
 from simmate.toolkit.creators.structure.base import StructureCreator
 
 
 class PyXtalStructure(StructureCreator):
+    """
+    Uses PyXtal to generate structures.
 
-    # see docs: https://pyxtal.readthedocs.io/en/latest/index.html
+    PyXtal is an open source Python library for the ab-initio generation of
+    random crystal structures.
+
+    see their home page: https://pyxtal.readthedocs.io/en/latest/index.html
+    """
 
     def __init__(
         self,
-        composition,  # must be a pymatgen Composition object,
-        volume_factor=1.0,
-        default_lattice=None,
-        tolerance_matrix=None,  # options unique to PyXtal's random_crystal
-        spacegroup_include=range(1, 231),
-        spacegroup_exclude=[],
+        composition: Composition,
+        volume_factor: float = 1.0,
+        default_lattice: list[list[float]] = None,
+        tolerance_matrix: list[list[float]] = None,
+        spacegroup_include: list[int] = range(1, 231),
+        spacegroup_exclude: list[int] = [],
     ):
 
-        #!!! this is inside the init because not all users will have this installed!
         try:
             from pyxtal import pyxtal
         except ModuleNotFoundError:
-            #!!! I should raise an error in the future
-            raise Exception("You must have PyXtal installed to use PyXtalStructure!!")
+            raise ModuleNotFoundError(
+                "You must have PyXtal installed to use PyXtalStructure. "
+                "Install it with 'pip install pyxtal'"
+            )
 
         # save the module for reference below
         self.pyxtal = pyxtal()
 
-        # save the composition information in the format that PyXtal wants ['Ca', 'N']
+        # save the composition information in the format that PyXtal
+        # wants ['Ca', 'N']
         self.species = [element.symbol for element in composition.elements]
         # save the number of Ions in the format that PyXtal wants [3,2]
         self.numIons = [
@@ -37,15 +48,14 @@ class PyXtalStructure(StructureCreator):
         ]
         # save the volume factor to be used later
         self.volume_factor = volume_factor
-        # if the user supplied a lattice, it should be a 3x3 matrix -- NOT a pymatgen lattice object
-        #!!! should I add a check that this is a matrix here?
+        # if the user supplied a lattice, it should be a 3x3 matrix -- NOT a
+        # pymatgen lattice object
         self.default_lattice = default_lattice
-        #!!! TO-DO: I need to allow the user to specify the tolerance matrix
+        # TODO: I need to allow the user to specify the tolerance matrix
         if tolerance_matrix:
-            print(
-                "Simmate's wrapper for PyXtal does not currently support setting "
-                "the tolerance matrix for PyXtal's random_crystal(). If you want this "
-                "option added, please contact the Simmate developers."
+            logging.warning(
+                "Simmate's wrapper for PyXtal does not currently support "
+                "setting the tolerance matrix for PyXtal's random_crystal()."
             )
 
         # make a list of spacegroups that we are allowed to choose from
@@ -56,7 +66,9 @@ class PyXtalStructure(StructureCreator):
         # our composition, so this list is updated as attempts fail in the
         # create_structure method.
 
-    def create_structure(self, spacegroup=None):
+    def create_structure(self, spacegroup: int = None) -> Structure:
+
+        from pyxtal.msg import Comp_CompatibilityError
 
         # If a spacegroup is not specified, grab a random one from our options.
         # The requested_spacegroup let's us know below if the method wanted a
@@ -69,36 +81,37 @@ class PyXtalStructure(StructureCreator):
         else:
             requested_spacegroup = True
 
-        from pyxtal.msg import Comp_CompatibilityError
-
         try:
-            # now make the new structure using PyXtal's crystal.random_crystal function
-            # NOTE: all of these options are set in the init except for the spacegroup
-            # No structure is returned. Instead they just attach it to the pyxtal object
+            # Now make the new structure using PyXtal's crystal.random_crystal
+            # function. Note, all of these options are set in the init except
+            # for the spacegroup. No structure is returned. Instead they just
+            # attach it to the pyxtal object
             self.pyxtal.from_random(
-                group=int(
-                    spacegroup
-                ),  # !!! why do I need int() here? This should already work without it...
+                group=int(spacegroup),
                 species=self.species,
                 numIons=self.numIons,
                 factor=self.volume_factor,
                 lattice=self.default_lattice,
+                # tm=<pyxtal.crystal.Tol_matrix object> # not supported yet
             )
-            # tm=<pyxtal.crystal.Tol_matrix object> # not supported right now!
+
         except Comp_CompatibilityError as exception:
             # if the user wanted this spacegroup, raise the error
             if requested_spacegroup:
                 raise exception
             # otherwise remove the spacegroup from our allowed list
             self.spacegroup_options.remove(spacegroup)
-            # And retry! Note: recursion is a shortcut here, but shouldn't be an issue
+            # And retry. Note: recursion is a shortcut here, but shouldn't
+            # be an issue
             self.create_structure()
 
         # note that we have a PyXtal structure object, not a PyMatGen one!
-        # The PyXtal structure has a *.valid feature that lets you know if structure creation was a success
+        # The PyXtal structure has a *.valid feature that lets you know if
+        # structure creation was a success
         # so we will check that
         if self.pyxtal.valid:
-            # if it is valid, we want the PyMatGen structure object, which is at *.struct
+            # if it is valid, we want the PyMatGen structure object, which is
+            # at *.struct
             return self.pyxtal.to_toolkit()
         else:
             # if it is not valid, the structure creation failed
