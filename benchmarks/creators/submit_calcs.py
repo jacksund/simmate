44353193a17b1600d3439ca4893c25dab88becc2
Jack Sundberg <jacksundberg123@gmail.com>
2022-09-12 13:25:04 -0400

    begin running relaxations

---
+++ benchmarks/creators/submit_calcs.py
@@ -0,0 +1,66 @@
+# -*- coding: utf-8 -*-
+
+from rich.progress import track
+
+from simmate.toolkit import Composition, Structure
+from simmate.utilities import get_directory
+from simmate.workflows.utilities import get_workflow
+
+COMPOSITIONS_TO_TEST = [
+    "Fe1",
+    "Si2",
+    "C4",
+    "Ti2O4",
+    "Si4O8",
+    "Al4O6",
+    "Si4N4O2",
+    "Sr4Si4N8",
+    "Mg4Si4O12",
+]
+compositions = [Composition(c) for c in COMPOSITIONS_TO_TEST]
+
+NSAMPLES_PER_COMPOSITION = 500
+
+CREATORS_TO_TEST = [
+    "Simmate",
+    # "XtalOpt",
+    # "ASE",
+    # "PyXtal",
+    # "GASP",
+    # "AIRSS",
+    # "USPEX",
+]
+
+parent_dir = get_directory("creator_benchmarks")
+workflow_static = get_workflow("static-energy.vasp.quality04")
+workflow_relax = get_workflow("relaxation.vasp.staged")
+
+for creator_name in CREATORS_TO_TEST:
+
+    for composition in compositions:
+        directory = parent_dir / creator_name / str(composition)
+
+        if not directory.exists():
+            continue
+
+        for file in track(directory.iterdir()):
+            structure = Structure.from_file(file)
+
+            workflow_static.run_cloud(
+                structure=structure,
+                source={
+                    "creator": creator_name,
+                    "file": str(file),
+                    "is_initial": True,
+                },
+                command="mpirun -n 4 vasp_std > vasp.out",
+            )
+            
+            workflow_relax.run_cloud(
+                structure=structure,
+                source={
+                    "creator": creator_name,
+                    "file": str(file),
+                },
+                command="mpirun -n 4 vasp_std > vasp.out",
+            )
