4a96a66fc2fc8fa5ce4c7203d7adca8c3011886f
Jack Sundberg <jacksundberg123@gmail.com>
2022-10-08 14:05:46 -0400

    add tournament selection

---
+++ src/simmate/toolkit/structure_prediction/evolution/selectors/tournament.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+
+import pandas
+
+from simmate.toolkit.structure_prediction.evolution.selectors import Selector
+
+
+class TournamentSelection(Selector):
+    """
+    Tournament selection selects N parents randomly and then choses the single
+    best of these those randomly selected. If multiple individuals are requested,
+    a separate tournament is used to select each.
+    """
+
+    @staticmethod
+    def select(
+        nselect: int,
+        individuals: pandas.DataFrame,
+        fitness_column: str,
+        # The percent of individuals participating in each tournament
+        tournament_size: float = 0.20,
+        tournament_min: int = 3,
+    ) -> pandas.DataFrame:
+
+        # We want the tournament size to be based on the total number of
+        # inidividuals available.
+        ntournament = int(len(individuals) * tournament_size)
+        if ntournament < tournament_min:
+            ntournament = tournament_min
+
+        # Run the series of tournaments and select the winner from each
+        tournament_winners = []
+        for n in range(nselect):
+            df_tourn = individuals.sample(ntournament, replace=False)
+            winner_id = df_tourn.nsmallest(1, fitness_column).iloc[0].id
+            tournament_winners.append(winner_id)
+
+        # take the winning ids and grab their enties
+        df_parents = individuals[individuals.id.isin(tournament_winners)]
+
+        return df_parents
