f9cbf221c14c6edaf2a0734383cf9fc806f077f4
Siona Benjamin <sionab@live.unc.edu>
2022-10-19 14:36:27 -0400

    add iterative training to build_from_table
    correct chunk_list method!!

--- src/simmate/calculators/deepmd/workflows/build_from_table.py
+++ src/simmate/calculators/deepmd/workflows/build_from_table.py
@@ -8,8 +8,6 @@ from simmate.website.workflows import models as all_datatables
 from simmate.workflow_engine import Workflow
 from simmate.workflows.utilities import get_workflow
 
-# from simmate.database.workflow_results import Dynamics
-
 
 class MlPotential__Deepmd__BuildFromTable(Workflow):
 
@@ -20,6 +18,9 @@ class MlPotential__Deepmd__BuildFromTable(Workflow):
         directory: Path,
         table_name: str,
         filter_kwargs: dict = {},
+        md_kwargs: dict = {},
+        #deepmd_kwargs: dict ={},
+        training_iterations: int = 1, #setting to 1 means no iterative training 
         **kwargs,
     ):
 
@@ -33,10 +34,12 @@ class MlPotential__Deepmd__BuildFromTable(Workflow):
             datatable = getattr(all_datatables, table_name)
         else:
             raise Exception("Unknown table name provided")
-        #
+        
+        #sort db structures by energy per atom
         all_db_structures = datatable.objects.filter(
             formula_reduced=composition.reduced_formula, **filter_kwargs
-        )
+        ).order_by('energy_per_atom')
+        
         # TODO:
         # - make a get_table utility
         # - check that the table has both Thermo and Forces mix-ins
@@ -49,19 +52,47 @@ class MlPotential__Deepmd__BuildFromTable(Workflow):
                 "Either populate your table with calculations or change "
                 "your filtering criteria."
             )
-
+        
+        #set the directory to run deepmd in 
+        deepmd_directory = directory / "deepmd"
+        
+        # grab the deepmd training workflow
+        deepmd_workflow = get_workflow("ml-potential.deepmd.train-model")
+        
+        #divide overall query into equal sized list of structures 
+        num_structs = all_db_structures.count()/training_iterations
+            
+        structure_lists = chunk_list(all_db_structures, num_structs)
+            
+        #have list of training_data and testing_data
+        training_data = []
+        testing_data = []
+            
+        for n, item in structure_lists:
+            #create training/testing files for each chunk of strutures 
+            train, test = DeepmdDataset.to_file(
+                ionic_step_structures=item,
+                directory = directory / f"deepmd_data_{n}")
+                
+            training_data.append(train)
+            testing_data.append(test)
+                
+            deepmd_workflow.run(
+                directory=deepmd_directory,
+                composition=composition,
+                command=f'eval "$(conda shell.bash hook)"; conda activate deepmd; dp train input{n}.json > deepmd.out',
+                training_data=training_data,
+                testing_data=testing_data,
+                    )
+                
+        
+        '''      
         # write structure data to files for use with deepmd
         training_data, testing_data = DeepmdDataset.to_file(
             ionic_step_structures=all_db_structures,
             directory=directory / "deepmd_data",
-        )
-        # TODO: consider splitting into separate datasets and training with
-        # data in stages
-
-        deepmd_directory = directory / "deepmd"
-
-        # grab the deepmd training workflow and run the first step
-        deepmd_workflow = get_workflow("ml-potential.deepmd.train-model")
+            )
+            
 
         deepmd_workflow.run(
             directory=deepmd_directory,
@@ -71,3 +102,4 @@ class MlPotential__Deepmd__BuildFromTable(Workflow):
             testing_data=testing_data,
             # TODO: consider making a subdirectory for deepmd
         )
+        '''
