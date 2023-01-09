210def96afcc460c747aa6f63aca8e5b70c10e24
Jack Sundberg <jack.sundberg@corteva.com>
2023-01-09 12:42:50 -0500

    fix neb writing method

--- src/simmate/toolkit/diffusion/distinct_path_finder.py
+++ src/simmate/toolkit/diffusion/distinct_path_finder.py
@@ -9,7 +9,7 @@ class DistinctPathFinder(PymatgenDPF):
     def write_all_migration_hops(self, directory: Path):
         # We write all the path files so users can visualized them if needed
         filename = directory / "all_migration_hops.cif"
-        self.write_all_paths(filename, nimages=10)
+        self.write_all_paths(str(filename), nimages=10)
         migration_hops = self.get_paths()
         for i, migration_hop in enumerate(migration_hops):
             number = str(i).zfill(2)  # converts numbers like 2 to "02"
