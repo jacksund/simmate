e29fcf2ba14bfaee85d2998b49845c5e73561383
Jack Sundberg <jacksundberg123@gmail.com>
2023-01-10 20:33:44 -0500

    begin renaming calculators module to apps

---
+++ src/simmate/apps/configs.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+from django.apps import AppConfig
+
+
+class BaderConfig(AppConfig):
+    name = "simmate.apps.bader"
+
+
+class CleaseConfig(AppConfig):
+    name = "simmate.apps.clease"
+
+
+class VaspConfig(AppConfig):
+    name = "simmate.apps.vasp"
+
+
+class EvoSearchConfig(AppConfig):
+    name = "simmate.apps.evo_search"
