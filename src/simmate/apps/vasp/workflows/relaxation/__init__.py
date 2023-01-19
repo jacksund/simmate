a0da27d974b030504c3ff8e6cf8777df32379fe0
SWeav02 <118130113+SWeav02@users.noreply.github.com>
2023-01-19 15:53:43 -0500

    Add PBEsol and HSEsol to simmate workflows
    import workflows from their modules

--- src/simmate/apps/vasp/workflows/relaxation/__init__.py
+++ src/simmate/apps/vasp/workflows/relaxation/__init__.py
@@ -1,7 +1,9 @@
 # -*- coding: utf-8 -*-
 
 from .matproj import Relaxation__Vasp__Matproj
+from .matproj_hsesol import Relaxation__Vasp__MatprojPbesol
 from .matproj_hse import Relaxation__Vasp__MatprojHse
+from .matproj_hsesol import Relaxation__Vasp__MatprojHsesol
 from .matproj_metal import Relaxation__Vasp__MatprojMetal
 from .matproj_scan import Relaxation__Vasp__MatprojScan
 from .mit import Relaxation__Vasp__Mit
