e45d4f0ce682ab341142314f4645c3d9fe052b8f
SWeav02 <118130113+SWeav02@users.noreply.github.com>
2023-01-19 18:52:26 -0500

    add PBEsol and HSEsol workflows 🎆  (#365)

--- src/simmate/apps/vasp/workflows/relaxation/__init__.py
+++ src/simmate/apps/vasp/workflows/relaxation/__init__.py
@@ -2,7 +2,9 @@
 
 from .matproj import Relaxation__Vasp__Matproj
 from .matproj_hse import Relaxation__Vasp__MatprojHse
+from .matproj_hsesol import Relaxation__Vasp__MatprojHsesol
 from .matproj_metal import Relaxation__Vasp__MatprojMetal
+from .matproj_pbesol import Relaxation__Vasp__MatprojPbesol
 from .matproj_scan import Relaxation__Vasp__MatprojScan
 from .mit import Relaxation__Vasp__Mit
 from .mvl_grainboundary import Relaxation__Vasp__MvlGrainboundary
