dd0a4356c92bf472fb3b93be71b2cbddb823b1b5
Sam Weaver <sweav@unc.edu>
2023-01-24 13:44:56 -0500

    Add PBEsol and HSEsol population-analysis

--- src/simmate/apps/vasp/workflows/population_analysis/__init__.py
+++ src/simmate/apps/vasp/workflows/population_analysis/__init__.py
@@ -4,8 +4,44 @@ from .badelf import (
     PopulationAnalysis__VaspBader__BadelfMatproj,
     StaticEnergy__Vasp__PrebadelfMatproj,
 )
+from .badelf_hse import (
+    PopulationAnalysis__VaspBader__BadelfMatprojHse,
+    StaticEnergy__Vasp__PrebadelfMatprojHse,
+)
+from .badelf_hsesol import (
+    PopulationAnalysis__VaspBader__BadelfMatprojHsesol,
+    StaticEnergy__Vasp__PrebadelfMatprojHsesol,
+)
+from .badelf_pbesol import (
+    PopulationAnalysis__VaspBader__BadelfMatprojPbesol,
+    StaticEnergy__Vasp__PrebadelfMatprojPbesol,
+)
+from .badelf_scan import (
+    PopulationAnalysis__VaspBader__BadelfMatprojScan,
+    StaticEnergy__Vasp__PrebadelfMatprojScan,
+)
 from .bader import (
     PopulationAnalysis__VaspBader__BaderMatproj,
     StaticEnergy__Vasp__PrebaderMatproj,
 )
+from .bader_hse import (
+    PopulationAnalysis__VaspBader__BaderMatprojHse,
+    StaticEnergy__Vasp__PrebaderMatprojHse,
+)
+from .bader_hsesol import (
+    PopulationAnalysis__VaspBader__BaderMatprojHsesol,
+    StaticEnergy__Vasp__PrebaderMatprojHsesol,
+)
+from .bader_pbesol import (
+    PopulationAnalysis__VaspBader__BaderMatprojPbesol,
+    StaticEnergy__Vasp__PrebaderMatprojPbesol,
+)
+from .bader_scan import (
+    PopulationAnalysis__VaspBader__BaderMatprojScan,
+    StaticEnergy__Vasp__PrebaderMatprojScan,
+)
 from .elf import PopulationAnalysis__Vasp__ElfMatproj
+from .elf_hse import PopulationAnalysis__Vasp__ElfMatprojHse
+from .elf_hsesol import PopulationAnalysis__Vasp__ElfMatprojHsesol
+from .elf_pbesol import PopulationAnalysis__Vasp__ElfMatprojPbesol
+from .elf_scan import PopulationAnalysis__Vasp__ElfMatprojScan
