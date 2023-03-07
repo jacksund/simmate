029592d0f04848cfe5ecbf79ef2e325c5ce28076
Siona Benjamin <sionab@live.unc.edu>
2023-03-07 14:37:41 -0500

    fix init

--- src/simmate/calculators/deepmd/workflows/__init__.py
+++ src/simmate/calculators/deepmd/workflows/__init__.py
@@ -2,13 +2,13 @@
 
 from .build_from_md import MlPotential__Deepmd__BuildFromMd
 from .build_from_table import MlPotential__Deepmd__BuildFromTable
-from .build_from_parallel import MlPotential__Deepmd_BuildFromParallel
+from .build_from_parallel import MlPotential__Deepmd__BuildFromParallel
 from .freeze_model import MlPotential__Deepmd__FreezeModel
 from .get_force_energy import MlPotential__Deepmd__Prediction
 from .run_lammps import MlPotential__Deepmd__RunLammps
 from .train_model import MlPotential__Deepmd__TrainModel
 from .run_ase_md import MlPotential__Deepmd__AseMD
-from .run_ase_optimize import MlPotential__Deepmd_AseOptimize
+from .run_ase_optimize import MlPotential__Deepmd__AseOptimize
 
 
 
