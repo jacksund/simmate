6ab11af179484e2b03e59e5442454dd7005b0e8b
Jack Sundberg <jack.sundberg@corteva.com>
2023-03-26 18:56:29 -0400

    add CSRF env variable

--- src/simmate/configuration/django/settings.py
+++ src/simmate/configuration/django/settings.py
@@ -55,6 +55,11 @@ DEBUG = os.getenv("DEBUG", "False") == "True"
 # enviornment variable isn't set yet, then we just defaul to the localhost.
 ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS", "127.0.0.1,localhost").split(",")
 
+# BUG-FIX: Django-unicorn ajax requests sometimes come from the server-side
+# ingress (url for k8s) or a nginx load balancer. To get past a 403 forbidden
+# result, we need to sometimes specify allowed origins.
+CSRF_TRUSTED_ORIGINS = os.getenv("DJANGO_CSRF_TRUSTED_ORIGINS", "localhost").split(",")
+
 # This is for setting the database via an connection URL. This is done as
 # an environment variable to allow setup with DigitalOcean
 DATABASE_URL = os.getenv("DATABASE_URL", None)
@@ -465,6 +470,3 @@ LOGOUT_REDIRECT_URL = "/accounts/loginstatus/"
 # Read more at...
 # https://docs.djangoproject.com/en/4.0/topics/async/#async-safety
 os.environ["DJANGO_ALLOW_ASYNC_UNSAFE"] = "true"
-
-# Debugging django-unicorn
-CSRF_TRUSTED_ORIGINS = ["http://*"]
