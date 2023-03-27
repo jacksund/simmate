8913606bd47a3559117d16cf8645be5c071b92c0
Jack Sundberg <jack.sundberg@corteva.com>
2023-03-27 14:59:55 -0400

    fix csrf env bug

--- src/simmate/configuration/django/settings.py
+++ src/simmate/configuration/django/settings.py
@@ -58,7 +58,9 @@ ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS", "127.0.0.1,localhost").split("
 # BUG-FIX: Django-unicorn ajax requests sometimes come from the server-side
 # ingress (url for k8s) or a nginx load balancer. To get past a 403 forbidden
 # result, we need to sometimes specify allowed origins.
-CSRF_TRUSTED_ORIGINS = os.getenv("DJANGO_CSRF_TRUSTED_ORIGINS", "localhost").split(",")
+CSRF_TRUSTED_ORIGINS = os.getenv(
+    "DJANGO_CSRF_TRUSTED_ORIGINS", "http://localhost",
+).split(",")
 
 # This is for setting the database via an connection URL. This is done as
 # an environment variable to allow setup with DigitalOcean
