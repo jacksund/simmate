23024561e480d10543ee3f0cbfc72884b41250ff
Jack Sundberg <jacksundberg123@gmail.com>
2024-01-04 10:45:37 -0500

    patch release bugs 🐛  (#583)

--- src/simmate/apps/badelf/core/__init__.py
+++ src/simmate/apps/badelf/core/__init__.py
@@ -3,6 +3,5 @@
 from .badelf import BadElfToolkit
 from .electride_finder import ElectrideFinder
 from .grid import Grid
-from .henkelman_bader_interface import ZeroFluxToolkit
 from .partitioning import PartitioningToolkit
 from .voxel_assignment import VoxelAssignmentToolkit
--- src/simmate/apps/warren_lab/workflows/relaxation/__init__.py
+++ src/simmate/apps/warren_lab/workflows/relaxation/__init__.py
@@ -2,10 +2,6 @@
 
 from .hse import Relaxation__Vasp__WarrenLabHse
 from .hsesol import Relaxation__Vasp__WarrenLabHsesol
-from .mit import Relaxation__Vasp__Mit
-from .mvl_grainboundary import Relaxation__Vasp__MvlGrainboundary
-from .mvl_neb_endpoint import Relaxation__Vasp__MvlNebEndpoint
-from .mvl_slab import Relaxation__Vasp__MvlSlab
 from .pbe import Relaxation__Vasp__WarrenLabPbe
 from .pbe_metal import Relaxation__Vasp__WarrenLabPbeMetal
 from .pbesol import Relaxation__Vasp__WarrenLabPbesol
