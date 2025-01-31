a1ab424ee7253c8ea769acf8cddf549faa7f2ee5
Sam Weaver <sweav@unc.edu>
2025-01-31 10:04:01 -0500

    Remove mp-pyrho exception

--- src/simmate/apps/badelf/__init__.py
+++ src/simmate/apps/badelf/__init__.py
@@ -1,10 +1,9 @@
 # -*- coding: utf-8 -*-
 try:
     import pybader
-    import pyrho
 except:
     raise Exception(
-        "Missing app-specific dependencies. Make sure to read our installation guide. "
-        "The `badelf` app requires two additional dependencies: `pybader` and `mp-pyrho`."
-        "Install these with `conda install -c conda-forge pybader mp-pyrho`"
+        "Missing app-specific dependencies. Make sure to read our installation guide."
+        "The `badelf` app requires one additional dependency: `pybader`."
+        "Install these with `conda install -c conda-forge pybader`"
     )
