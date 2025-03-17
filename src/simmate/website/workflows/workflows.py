5d35cbde653a02062e4df7d4c76b698ef8788088
Jack Sundberg <jack.sundberg@corteva.com>
2025-03-17 09:30:24 -0500

    register util workflows

--- src/simmate/website/workflows/workflows.py
+++ src/simmate/website/workflows/workflows.py
@@ -1,4 +1,6 @@
 # -*- coding: utf-8 -*-
 
+# default workflows that I don't have better place for yet. consider deprec:
+
 from simmate.workflows.base_flow_types.customized import Customized__Toolkit__UserConfig
 from simmate.workflows.base_flow_types.restart import Restart__Toolkit__Automatic
--- src/simmate/workflows/base_flow_types/customized.py
+++ src/simmate/workflows/base_flow_types/customized.py
@@ -113,8 +113,6 @@ class Customized__Toolkit__UserConfig(Workflow):
         input_parameters.pop("source")
 
         # now run the task with the remaining parameters
-        # Note, we are using run_config instead of run because we do not want this
-        # registered as a task run of the original class
         state = NewWorkflow.run(
             source={
                 "customized": True,
