ad53f0de902de8b260a6ebe866b7888c9e9a0ec5
Jack Sundberg <jack.sundberg@corteva.com>
2025-10-08 12:23:33 -0500

    update conda envs and ci

--- src/simmate/__init__.py
+++ src/simmate/__init__.py
@@ -1,5 +1,29 @@
 # -*- coding: utf-8 -*-
 
+# This line just disables anything var from being set as an attribute to the
+# simmate module (e.g., it disables `from simmate import RichHandler`)
+__all__ = []
+
+try:
+    # because we now only support conda-install, do a quick check for our primary
+    # 3 dependencies. If any are missing, it is likely the user installed via
+    # pip where we didnt list *any* of our dependencies
+    import django
+    import pymatgen
+    import rdkit
+except:
+    raise Exception(
+        "Simmate's core dependencies were not found. "
+        "You most likely are seeing this message if you improperly installed simmate "
+        "using `pip` or `uv`. "
+        "To fix this, make sure you install simmate using conda and in a clean env. "
+        "We only support `conda` installs because some of our dependencies "
+        "(such as rdkit) only consistently work when installed via conda-forge. "
+        "We hope this will change in the future and we're sorry for "
+        "the inconvenience!"
+    )
+
+
 import importlib.metadata
 import logging
 
