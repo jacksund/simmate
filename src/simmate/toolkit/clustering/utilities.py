a92478f6fb9f77db77e85537ca01d56380c54548
Jack Sundberg <jack.sundberg@corteva.com>
2025-10-30 14:09:31 -0500

    update util

--- src/simmate/toolkit/clustering/utilities.py
+++ src/simmate/toolkit/clustering/utilities.py
@@ -16,11 +16,15 @@ def flatten_clusters(clusters: list[list]) -> list[int]:
     return column_ids
 
 
-def unflatten_cluster_ids(clusters: list[list]) -> list[int]:
+def unflatten_cluster_ids(cluster_ids: list[int]) -> list[list]:
     """
     Reverse of flatten_cluster_output
 
     Takes a flat list of cluster ids like [1, 1, 2, 2, ....] and groups them
     into clusters of the form [[mol1, mol2, ....], [mol3, mol4, ...], ...]
     """
-    pass
+    n_clusters = max(cluster_ids) + 1  # bc counting starts at 0
+    clusters = [[] for _ in range(n_clusters)]
+    for entry_index, cluster_id in enumerate(cluster_ids):
+        clusters[cluster_id].append(entry_index)
+    return clusters
