f894419972f03ea664ab5f4518f3e2daaef7676b
Jack Sundberg <jacksundberg123@gmail.com>
2026-04-02 21:33:45 -0500

    update pdb data load

--- src/simmate/apps/pdb/models/ligands.py
+++ src/simmate/apps/pdb/models/ligands.py
@@ -1,11 +1,9 @@
 # -*- coding: utf-8 -*-
 
-import gzip
 import json
 import logging
 import urllib
 from datetime import datetime
-from pathlib import Path
 
 import pandas
 import requests
@@ -14,6 +12,7 @@ from pymatgen.io.cif import CifFile
 from rich.progress import track
 
 from simmate.apps.rdkit.models import Molecule
+from simmate.config import settings
 from simmate.database.core import table_column
 from simmate.database.mixins import ThirdPartyData
 from simmate.toolkit import Molecule as ToolkitMolecule
@@ -24,8 +23,7 @@ class PdbLigand(ThirdPartyData, Molecule):
     Small molecules from the [Protein Data Bank (PDB)](https://www.wwpdb.org/).
 
     The small molecules are loaded from the "chemical component dictionary"
-    at https://www.wwpdb.org/data/ccd. These molecules are also available as
-    bulk downloads from http://ligand-expo.rcsb.org/ld-download.html
+    at https://www.wwpdb.org/data/ccd.
     """
 
     class Meta:
@@ -36,246 +34,154 @@ class PdbLigand(ThirdPartyData, Molecule):
     is_redistribution_allowed = True
 
     id = table_column.CharField(max_length=5, primary_key=True)
-    """
-    The id used to represent the molecule (ex: "ZZZ")
-    """
+    """The id used to represent the molecule (ex: "ZZZ")"""
 
     name = table_column.TextField()
-    """
-    Chemical name of the molecule. All names are converted to full lower-case
-    """
+    """Chemical name of the molecule."""
 
     type_code = table_column.TextField()
-    """
-    A preliminary classification used by PDB (ex: "HETAIN")
-    """
-    # _chem_comp.pdbx_type
+    """A preliminary classification used by PDB (ex: "HETAIN")"""
 
-    type = table_column.TextField()
-    """
-    The type of monomer (ex: "NON-POLYMER")
-    """
-    # _chem_comp.type
+    monomer_type = table_column.TextField()
+    """The type of monomer (ex: "NON-POLYMER")"""
 
-    created_at = table_column.DateTimeField(
-        blank=True,
-        null=True,
-        db_index=True,
-    )
-    """
-    Timestamp of when this row was first added to the PDB database
-    """
-    # _chem_comp.pdbx_initial_date
+    created_at_original = table_column.DateTimeField(blank=True, null=True, db_index=True)
+    """Timestamp of when this row was first added to the PDB database"""
 
-    updated_at = table_column.DateTimeField(
-        blank=True,
-        null=True,
-        db_index=True,
-    )
-    """
-    Timestamp of when this row was was lasted changed / updated by the PDB database
-    """
-    # _chem_comp.pdbx_modified_date
+    updated_at_original = table_column.DateTimeField(blank=True, null=True, db_index=True)
+    """Timestamp of when this row was was lasted changed / updated by the PDB"""
 
-    protein_ids = table_column.JSONField(blank=True, null=True)
-    """
-    List of PDB protein IDs where this ligand is found
-    """
-    # TODO: this should become a foreign key to PdbProtein table in the future
+    protein_ids_original = table_column.JSONField(blank=True, null=True)
+    """List of PDB protein IDs where this ligand is found"""
 
     @property
     def external_link(self) -> str:
-        """
-        URL to this molecule in the "PDB Europe" website.
-        """
-        # example URL for ligand: https://www.ebi.ac.uk/pdbe-srv/pdbechem/chemicalCompound/complete/123
-        # example URL for protein: https://www.ebi.ac.uk/pdbe/entry/pdb/1gi5
         return f"https://www.ebi.ac.uk/pdbe-srv/pdbechem/chemicalCompound/complete/{self.id}"
 
-    # -------------------------------------------------------------------------
-
     @classmethod
-    def load_source_data(cls):
+    def load_source_data(cls, include_protein_ids: bool = False):
         """
-        This link gives directions for archive locations:
+        Downloads and loads all ligand data from the PDB.
 
-        - http://www.wwpdb.org/ftp/pdb-ftp-sites
+        #### Parameters
 
-        And it tells us (i) where all small molecules (ligands) are stored and (ii) a JSON file to download a list of all of the molecule IDs:
-
-        i.  https://files.wwpdb.org/pub/pdb/refdata/chem_comp/
-        ii. https://files.wwpdb.org/pub/pdb/holdings/refdata_id_list.json.gz
-
-        The same is done for PDB files:
-
-        i.  https://files.wwpdb.org/pub/pdb/data/structures/all/mmCIF/{pdb_id}.cif.gz  # <-- note the {}
-        ii. https://files.wwpdb.org/pub/pdb/holdings/current_file_holdings.json.gz
-
-        Because the chem_comp (ligands) is a much smaller dataset, they also provide a bulk
-        download option instead of the links above:
-
-        - https://www.wwpdb.org/data/ccd
-        - https://files.wwpdb.org/pub/pdb/data/monomers/components.cif
-
-
-
-        Separately, I found directions on downloading ligands that might be helpful in the future:
-
-        1. Go to https://www.rcsb.org/docs/programmatic-access/file-download-services
-        2. ctrl+F "ligand" to find the "Small molecule files" section
-        3. select format type (but idk how to iterate...)
-
-        There are also batch download scripts, but these look to be intended for full
-        protein files, while we only want small molecules for now. I don't use these scripts
-        but instead write my own:
-
-        - https://www.rcsb.org/docs/programmatic-access/batch-downloads-with-shell-script
-
-
-
-        #### 07.01.2025
-
-        Looks like the Ligand Expo has been retired. I'm using these links now:
-        - https://www.wwpdb.org/data/ccd
-        - https://www.rcsb.org/search
-        - https://data.rcsb.org/
-        - https://github.com/rcsb/py-rcsb-api
-
+        - `include_protein_ids`: whether to fetch the list of PDB protein IDs
+            where each ligand is found. This is a VERY slow process as it
+            requires a separate API call for every ligand. Defaults to False.
         """
-        # There is no need for refdata_id_list.json like we did for proteins bc
-        # PDB provides a single bulk download file for us.
 
-        # download, uncompress, and read the listing to a dictionary
-        logging.info("Downloading ligands from PDB...")
-        data_filename = Path("components.cif")
+        # Ensure the config directory exists
+        pdb_config_dir = settings.config_directory / "pdb"
+        pdb_config_dir.mkdir(parents=True, exist_ok=True)
+
+        # 1. Download and read the listing of all ligands
+        data_filename = pdb_config_dir / "components.cif"
         if not data_filename.exists():
+            logging.info("Downloading ligands from PDB...")
             data_url = "https://files.wwpdb.org/pub/pdb/data/monomers/components.cif"
             urllib.request.urlretrieve(data_url, data_filename)
-        logging.info("Reading ligands file...")
+
+        logging.info("Reading ligands file (this may take a minute)...")
         data = CifFile.from_file(data_filename).data
 
-        logging.info("Downloading mappings from PDB...")
-        # DEPREC - ligand expo no longer exists.
-        # mapping_url = "http://ligand-expo.rcsb.org/dictionaries/cc-to-pdb.tdd"
-        # mapping_filename = Path("cc-to-pdb.tdd")
-        # if not mapping_filename.exists():
-        #     urllib.request.urlretrieve(mapping_url, mapping_filename)
-        # mappings = pandas.read_csv(
-        #     mapping_filename,
-        #     delimiter="\t",
-        #     header=None,
-        #     names=["ligand_id", "protein_ids"],
-        #     index_col="ligand_id",
-        # )
-        # protein_ids = mappings.loc[ligand_id].protein_ids.strip().split(" ")
-        data_filename = Path("cc_to_pdb_map.json")
-        if data_filename.exists():
-            with open(data_filename, "r") as f:
+        # 2. Map ligands to the proteins they are found in
+        # This is a VERY slow process if it hasn't been done before.
+        map_filename = pdb_config_dir / "cc_to_pdb_map.json"
+        if map_filename.exists():
+            with open(map_filename, "r") as f:
                 pdb_id_map = json.load(f)
-        else:
-            ligand_ids = [e.data.get("_chem_comp.id", None) for k, e in data.items()]
+        elif include_protein_ids:
+            logging.info("Fetching ligand-to-protein mappings (this is slow)...")
+            ligand_ids = [e.data.get("_chem_comp.id") for e in data.values()]
             pdb_id_map = {
-                ligand_id: cls._get_pdb_ids(ligand_id, silence_errors=True)
-                for ligand_id in track(ligand_ids)
+                lid: cls._get_pdb_ids(lid, silence_errors=True)
+                for lid in track(ligand_ids)
             }
-            with open(data_filename, "w") as f:
+            with open(map_filename, "w") as f:
                 json.dump(pdb_id_map, f)
+        else:
+            pdb_id_map = {}
 
-        # This for-loop simply loads all the metadata and smiles strings
+        # 3. Parse metadata and molecules
         logging.info("Parsing metadata and molecules...")
         db_objs = []
         failed = 0
-        for key, entry in track(data.items(), total=len(data)):
-            # TODO: skip if molecule has already been added.
-            # Maybe check the "last_modified" data too
-
-            # ~30 of the 42k molecule fail to load because they don't have OpenEye
-            # smiles listed. Another ~200 fail because of invalid smiles
+        for entry in track(data.values()):
             try:
                 # convert CIF object to python dict
                 entry_data = entry.data
 
-                # This section grabs the SMILES string from the CIF file
-                # and then converts to a Molecule.
-                comp_ids = entry_data.get("_pdbx_chem_comp_descriptor.comp_id")
-                descriptors = entry_data.get("_pdbx_chem_comp_descriptor.descriptor")
-                programs = entry_data.get("_pdbx_chem_comp_descriptor.program")
-                program_versions = entry_data.get(
-                    "_pdbx_chem_comp_descriptor.program_version"
-                )
-                types = entry_data.get("_pdbx_chem_comp_descriptor.type")
+                # grab the SMILES string and convert to a Molecule.
+                # All SMILES are stored in a table-like format in the CIF
                 df = pandas.DataFrame(
                     data={
-                        "comp_id": comp_ids,
-                        "descriptor": descriptors,
-                        "program": programs,
-                        "program_version": program_versions,
-                        "type": types,
+                        "type": entry_data.get("_pdbx_chem_comp_descriptor.type"),
+                        "program": entry_data.get("_pdbx_chem_comp_descriptor.program"),
+                        "smiles": entry_data.get(
+                            "_pdbx_chem_comp_descriptor.descriptor"
+                        ),
                     }
                 )
                 smiles_row = df[
                     (df.type == "SMILES_CANONICAL")
                     & (df.program == "OpenEye OEToolkits")
                 ].iloc[0]
-                smiles_str = smiles_row.descriptor
+                smiles_str = smiles_row.smiles
                 molecule = ToolkitMolecule.from_smiles(smiles_str)
 
                 # pull timestamps and convert to proper format
-                created_at = make_aware(
-                    datetime.strptime(
-                        entry_data.get("_chem_comp.pdbx_initial_date", None),
-                        "%Y-%m-%d",
-                    )
+                created_at_raw = entry_data.get("_chem_comp.pdbx_initial_date")
+                updated_at_raw = entry_data.get("_chem_comp.pdbx_modified_date")
+                created_at_original = (
+                    make_aware(datetime.strptime(created_at_raw, "%Y-%m-%d"))
+                    if created_at_raw
+                    else None
                 )
-                updated_at = make_aware(
-                    datetime.strptime(
-                        entry_data.get("_chem_comp.pdbx_modified_date", None),
-                        "%Y-%m-%d",
-                    )
+                updated_at_original = (
+                    make_aware(datetime.strptime(updated_at_raw, "%Y-%m-%d"))
+                    if updated_at_raw
+                    else None
                 )
 
-                # grab protein ids (and not all ligands have them)
-                ligand_id = entry_data.get("_chem_comp.id", None)
-                protein_ids = pdb_id_map.get(ligand_id, None)
-
-                # now finalize the metadata and molecule into a database object
+                # finalize the metadata and molecule into a database object
+                ligand_id = entry_data.get("_chem_comp.id")
                 db_obj = PdbLigand.from_toolkit(
                     molecule=molecule,
-                    molecule_original=smiles_str,  # consider setting to mmCIF
+                    molecule_original=smiles_str,
                     is_3d=False,
-                    # grab basic metadata
                     id=ligand_id,
-                    type_code=entry_data.get("_chem_comp.pdbx_type", None),
-                    type=entry_data.get("_chem_comp.type", None),
-                    name=entry_data.get("_chem_comp.name", None),
-                    created_at=created_at,
-                    updated_at=updated_at,
-                    protein_ids=protein_ids,
+                    type_code=entry_data.get("_chem_comp.pdbx_type"),
+                    monomer_type=entry_data.get("_chem_comp.type"),
+                    name=entry_data.get("_chem_comp.name"),
+                    created_at_original=created_at_original,
+                    updated_at_original=updated_at_original,
+                    protein_ids_original=pdb_id_map.get(ligand_id),
                 )
                 db_objs.append(db_obj)
-
-            except:
+            except Exception:
                 failed += 1
 
-        logging.info("Saving new rows to database...")
+        # 4. Save to database
+        logging.info(f"Saving {len(db_objs)} rows to database...")
         cls.objects.bulk_create(
             db_objs,
             batch_size=1000,
             ignore_conflicts=True,
         )
-
-        logging.info("Adding molecule fingerprints...")
-        cls.populate_fingerprint_database()
-
+        if settings.postgres_rdkit_extension:
+            cls.populate_fingerprint_database()
         logging.info("Done.")
         return failed
 
     @staticmethod
     def _get_pdb_ids(cc_id: str, silence_errors: bool = False):
-
+        """
+        Queries the RCSB Search API to find all PDB entries that contain
+        the given chemical component (ligand) ID.
+        """
+        # disable ssl warnings just in case the server has issues
         import urllib3
 
-        # to silence ssl warnings below
         urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
         url = "https://search.rcsb.org/rcsbsearch/v2/query"
@@ -292,58 +198,13 @@ class PdbLigand(ThirdPartyData, Molecule):
             },
             "return_type": "entry",
         }
-
-        response = requests.post(url, json=query, verify=False)  # ssl disabled
-
+        response = requests.post(url, json=query, verify=False)
         if response.ok:
             if response.status_code == 204:  # "No content" code
                 return []
             data = response.json()
-            pdb_ids = [result["identifier"] for result in data.get("result_set", [])]
-            return pdb_ids
+            return [result["identifier"] for result in data.get("result_set", [])]
+        elif silence_errors:
+            return None
         else:
-            if silence_errors:
-                return None
-            raise Exception(f"Request failed: {response.status_code}\n {response.text}")
-
-
-# -----------------------------------------------------------------------------
-
-# NOTE: this function is for loading proteins + metadata for when I want this
-# as its own table. For now, I just use http://ligand-expo.rcsb.org/dictionaries/cc-to-pdb.tdd
-# and store linked IDs. In the future this will be a foreign key field
-
-
-def _load_pdb_proteins(
-    id_listings_url: str = "https://files.wwpdb.org/pub/pdb/holdings/current_file_holdings.json.gz",
-    # check_local_first: bool = True,  # TODO
-):
-    id_listings_url = (
-        "https://files.wwpdb.org/pub/pdb/holdings/current_file_holdings.json.gz"
-    )
-
-    # download, uncompress, and read the listing to a dictionary
-    logging.info("Loading list of available protein IDs...")
-
-    local_filename = Path("current_file_holdings.json.gz")
-    urllib.request.urlretrieve(id_listings_url, local_filename)
-
-    with gzip.open(local_filename) as file:
-        id_listings = json.load(file)
-
-    logging.info("Done.")
-
-    # TODO: if update_only=True, only include IDs that aren't in our database yet
-
-    # grab all the URLs for downloading mmcif files from pdb
-    mmcif_locs = [v["mmcif"][0] for k, v in id_listings.items()]
-
-    base_url = "https://files.wwpdb.org/pub/"
-    for mmcif_loc in track(mmcif_locs[:100]):
-        full_url = base_url + mmcif_loc
-        mmcif_file = Path(mmcif_loc).name
-        urllib.request.urlretrieve(id_listings_url, mmcif_file)
-        breakpoint()  # TODO -- load
-
-        # delete file to saze on disk space
-        mmcif_file.unlink()
+            response.raise_for_status()
