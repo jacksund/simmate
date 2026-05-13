85214b7ba122151cd1ac82627a70db1a8c1bee74
Sam W. <sweav@unc.edu>
2026-05-13 17:35:05 -0400

    update init

--- src/simmate/apps/baderkit/models/__init__.py
+++ src/simmate/apps/baderkit/models/__init__.py
@@ -6,4 +6,4 @@ from .elf_labeler import ElfLabeler
 from .elf_features import ElfFeatures
 from .elf_radii import ElfRadii
 from .elf_radius import AllElfRadii, AtomElfRadii
-from .base import BaderkitAnalysis
+from .base import Baderkit
