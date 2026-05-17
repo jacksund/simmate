e643f276a82ff4b5d4ccef00d1d6394d51f0456c
SWeav02 <sweav@unc.edu>
2026-05-17 13:31:14 -0400

    Add fast electride search

--- src/simmate/apps/warren_lab/workflows/static_energy/pre_bader_badelf.py
+++ src/simmate/apps/warren_lab/workflows/static_energy/pre_bader_badelf.py
@@ -155,3 +155,31 @@ class StaticEnergy__Vasp__PreRadiiWarren(StaticEnergy__Vasp__PbesolWarren):
         LELF=True,  # write ELFCAR
         NPAR=1,  # must be set for ELF calc
     )
+
+class StaticEnergy__Vasp__PreFinderWarren(StaticEnergy__Vasp__PbesolWarren):
+    """
+    Runs a static energy calculation with a low-density FFT grid to quickly scan
+    for electride features.
+    
+    We do NOT recommend running this calculation on its own. Instead, you should
+    use the full workflow, which runs this calculation AND the following bader
+    analysis for you. This S3Task is only the first step of that workflow.
+
+    See `bader.workflows.materials_project`.
+    """
+
+    potcar_mappings = PBE_GW_POTCAR_MAPPINGS
+
+    # The key thing for bader analysis is that we need a very fine FFT mesh. Other
+    # than that, it's the same as a static energy calculation.
+    _incar_updates = dict(
+        NGX__density_a=10,  # set grid and fine grid to same, reasonably accurate value
+        NGY__density_b=10,
+        NGZ__density_c=10,
+        NGXF__density_a=10,
+        NGYF__density_b=10,
+        NGZF__density_c=10,
+        LAECHG=True,  # write core charge density to AECCAR0 and valence to AECCAR2
+        LELF=True,  # write ELFCAR
+        NPAR=1,  # must be set for ELF calc
+    )
