bbd6e205d30dd7a61d0251ff1cfd622be9638c91
Jack Sundberg <jacksundberg123@gmail.com>
2026-05-24 19:35:09 -0500

    patch fixed comp

--- src/simmate/apps/evolution/workflows/fixed_composition.py
+++ src/simmate/apps/evolution/workflows/fixed_composition.py
@@ -23,7 +23,7 @@ class StructurePrediction__Toolkit__FixedComposition(Workflow):
     def run_config(
         cls,
         composition: str | Composition,
-        subworkflow_name: str | Workflow = "static-energy.vasp.low-quality",
+        subworkflow_name: str | Workflow = "staged-relax-static.quantum-espresso.low-quality",
         subworkflow_kwargs: dict = {},
         fitness_field: str = "energy_per_atom",
         nfirst_generation: int = 15,
